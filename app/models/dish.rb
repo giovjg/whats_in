a34c2f0523c8fceb2d9e613d9219b5cc45a296e1
Luis Quinones <luis.quinones9@upr.edu>
2022-03-01 12:39:12 +0100

    restaurant validation

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,5 +2,5 @@ class Dish < ApplicationRecord
   belongs_to :restaurant
   has_many :ingredients, through: :dish_ingredients
   has_one_attached :photo
-  validates :name, presence: true, uniqueness: { scope: restaurant }
+  validates :name, presence: true, uniqueness: { scope: Restaurant }
 end
