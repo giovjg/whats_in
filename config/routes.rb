5133dd0c1eb455be0e59af98ab9c96fd8407b1cf
Luis Quinones <luis.quinones9@upr.edu>
2022-03-01 11:42:13 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :show ] do
+    resources :favourites, only: [:create]
+    resources :reviews, only: [ :index, :create ]
+  end
 end
