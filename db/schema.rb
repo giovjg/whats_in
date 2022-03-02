723b3eb01dac62018e96ff27eada4da78cdf8f1d
ghostly08 <steven.produccionbcn@gmail.com>
2022-03-02 16:32:00 +0100

    adding long and latitud to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_140441) do
+ActiveRecord::Schema.define(version: 2022_03_02_152712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2022_03_01_140441) do
     t.integer "end_time"
     t.boolean "adaptability"
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
