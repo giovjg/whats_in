699daad50e7f50c1189bc30587699eea884e9d72
ghostly08 <steven.produccionbcn@gmail.com>
2022-03-02 18:04:17 +0100

    add map

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,9 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   has_many :dishes
+  has_many :ingredients
   has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
