462c184d63c54a4b6f6deb75bd57862078e602ec
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-03-02 12:13:42 +0100

    show method in restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,9 @@
 class RestaurantsController < ApplicationController
+  # GET /restaurant/1
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   # def index
   #   if params[:ingredient]
   #     @restaurants = Restaurant.where.not(params[:ingredient])
@@ -7,6 +12,9 @@ class RestaurantsController < ApplicationController
   #   end
   # end
 
+  # private
+
+  # Only allow a list of trusted parameters through
   # def restaurant_params
   #   params.require(:resturant).permit(:name, :address, :photo)
   # end
