ed25ec3096f91101e557e5080e525946bd6fa0e2
Giovanna Jaramillo-Gutierrez <gjgutierrez@protonmail.com>
2022-03-07 12:49:22 +0100

    add favicon set up configuration and an image for testing purposes

---
+++ app/helpers/asset_tag_helper.rb
@@ -0,0 +1,10 @@
+def favicon_link_tag(source = "favicon.ico", options = {})
+  tag("link", {
+    rel: "shortcut icon",
+    type: "image/x-icon",
+    href: path_to_image(source, skip_pipeline: options.delete(:skip_pipeline))
+              }.merge!(options.symbolize_keys))
+end
+
+# favicon_link_tag
+# => <link href="/assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
