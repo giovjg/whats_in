bce056ceeecd8a08146b0ecdec7bd09e3567fcf0
Luis Quinones <luis.quinones9@upr.edu>
2022-03-08 17:35:15 +0100

    authentication for profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, only: :profile
   def default_url_options
     { host: ENV["https://www.whatsin.digital/"] || "localhost:3000" }
   end
